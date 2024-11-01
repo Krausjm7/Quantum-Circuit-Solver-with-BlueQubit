aa1f3e9a4116aa2f6ef607639d33f3bce7c60a82
Jamie Kraus <krausjm7@gmail.com>
2024-11-01 10:36:07 -0500

    Update Circuit_1_30qubits_Solver.py

--- Circuit_1_30qubits_Solver.py
+++ Circuit_1_30qubits_Solver.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 bq = bluequbit.init("YOUR_BLUEQUBIT_API_KEY")
 
 # Loading the circuit from the .qasm file
-qasm_file_path = "C:/Users/Jamie/BlueQubit Hackathon/circuit_1_30q_fixed.qasm"
+qasm_file_path = "YOUR_FOLDER_DIRECTORY_WHERE_CIRCUIT_FILES_ARE_LOCATED"
 with open(qasm_file_path, 'r') as f:
     qasm_content = f.read()
 
